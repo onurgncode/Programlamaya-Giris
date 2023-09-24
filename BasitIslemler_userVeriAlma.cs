e29344a9c840900e374cd09d68d87bf10b49e4b1
Onur Doğan <39799969+onurgncode@users.noreply.github.com>
2023-09-24 23:06:35 +0300

    Add files via upload

---
+++ BasitIslemler_userVeriAlma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Benim;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Ekrandan okunan iki tam sayı (int) için basit işlemler yaparak ekrana sonuçları basan kod yazınız. Bu işlemler toplama, çıkarma, çarpma, bölme ve kalan işlemleridir.
+        Console.WriteLine("Dört İşlem Problemi");
+        Console.WriteLine("----------------------");
+        Console.WriteLine("Birinci Sayıyı Giriniz");
+        int sayi1 = Convert.ToInt32( Console.ReadLine());
+        Console.Clear();
+        Console.WriteLine("İkinci Sayıyı Giriniz");
+        int sayi2 = Convert.ToInt32( Console.ReadLine());
+        Console.Clear();
+        Console.WriteLine($"Birinci Sayı ile ikinci sayının toplamı = {sayi1+sayi2}");
+        Console.WriteLine($"Birinci Sayı ile ikinci sayının Çarpımı = {sayi1*sayi2}");
+        Console.WriteLine($"Birinci Sayı ile ikinci sayının Bölümü = {sayi1/sayi2}");
+        Console.WriteLine($"Birinci Sayı ile ikinci sayının Farkı = {sayi1-sayi2}");
+
+
+    }
+}
